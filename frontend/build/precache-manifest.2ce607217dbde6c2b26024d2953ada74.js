bd5c4ba362fed4a85fb68a6d9782d1560e42d9b2
mockdone <690033956@qq.com>
2020-07-01 10:56:46 +0800

    直接将编译的包上传

---
+++ frontend/build/precache-manifest.2ce607217dbde6c2b26024d2953ada74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a2cf47861e95d0c66738f8aaaca0570",
+    "url": "./index.html"
+  },
+  {
+    "revision": "49f5b74f03d9fbdcd4fc",
+    "url": "./static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "49f5b74f03d9fbdcd4fc",
+    "url": "./static/js/2.d3d9c3b3.chunk.js"
+  },
+  {
+    "revision": "e3b7b462f7a354fc82aba5d068643821",
+    "url": "./static/js/2.d3d9c3b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ea89a0d9e3595a52ca0",
+    "url": "./static/js/main.fcf1f27e.chunk.js"
+  },
+  {
+    "revision": "512409973b16c486083e",
+    "url": "./static/js/runtime-main.edad0a55.js"
+  }
+]);
